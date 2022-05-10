a2fb8d6224378824130ffd3162bfc4e069d29c81
WebMaster <umrec_web@unimex.edu.mx>
2022-05-10 11:20:46 -0500

    code format

--- php/setCookies.php
+++ php/setCookies.php
@@ -7,26 +7,26 @@ UtmCookie::setName('campanas');
 UtmCookie::setOverwrite(false);
 // set lifetime to 3 Days
 UtmCookie::setLifetime(new DateInterval('P1Y'));
-$utmCookieSource = UtmCookie::get('utm_source');
+$utmCookieSource   = UtmCookie::get('utm_source');
 $utmCookieCampaign = UtmCookie::get('utm_campaign');
-$utmCookieId = UtmCookie::get('utm_id');
-$utmContent = UtmCookie::get('utm_content');
-$utmCookieMedium = UtmCookie::get('utm_medium');
-$utmCookieTerm = UtmCookie::get('utm_term');
+$utmCookieId       = UtmCookie::get('utm_id');
+$utmContent        = UtmCookie::get('utm_content');
+$utmCookieMedium   = UtmCookie::get('utm_medium');
+$utmCookieTerm     = UtmCookie::get('utm_term');
 
-$source  = $utmCookieSource;
+$source   = $utmCookieSource;
 $campaign = $utmCookieCampaign;                        
-$cId = $utmCookieId;
-$content = $utmContent; 
-$medium = $utmCookieMedium;
-$term = $utmCookieTerm;
+$cId      = $utmCookieId;
+$content  = $utmContent; 
+$medium  = $utmCookieMedium;
+$term    = $utmCookieTerm;
 
-$_SESSION["source"] = $source;
-$_SESSION["campana"] = $campaign;
+$_SESSION["source"]   = $source;
+$_SESSION["campana"]  = $campaign;
 $_SESSION["idcookie"] = $cId;
-$_SESSION["content"] = $content;
-$_SESSION["medium"] = $medium;
-$_SESSION["term"] = $term;
+$_SESSION["content"]  = $content;
+$_SESSION["medium"]   = $medium;
+$_SESSION["term"]     = $term;
 
 
 /*
