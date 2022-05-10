0bac71b190d301f0fc7c3729713657b5dfc5da3b
WebMaster <umrec_web@unimex.edu.mx>
2022-05-10 11:18:22 -0500

    get utm

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+
+require('php/setCookies.php');
+session_start();
+
+echo $_SESSION["source"]."<br>";
+echo $_SESSION["campana"]."<br>";
+echo $_SESSION["idcookie"]."<br>";
+echo $_SESSION["content"]."<br>";
+echo $_SESSION["medium"]."<br>";
+echo $_SESSION["term"]."<br>";
+
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Prueba index</title>
+</head>
+<body>
+    
+</body>
+</html>
